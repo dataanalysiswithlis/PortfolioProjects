42bff3587ef5e72c9b1d2679b23f7c31e0b61619
Lis Emmanuel <lisemmanuel@Liss-MBP.attlocal.net>
2022-07-31 16:35:54 -0500

    added comments

--- 120_yrs_OlympicHistory_DataExploration_SQL.sql
+++ 120_yrs_OlympicHistory_DataExploration_SQL.sql
@@ -83,12 +83,13 @@ select oe.games,count(distinct onr.region) as total_countries
 
 with highest_lowest_participating_countries as(select *, dense_rank() over(order by re.total_countries desc) max_rank, dense_rank() over(order by re.total_countries) min_rank
 		        from 
-		        (select oe.games, count(distinct onr.region) as total_countries
+		        (
+		        select oe.games, count(distinct onr.region) as total_countries
 				from olympic_events oe
 				join olympic_noc_regions onr 
 				on oe.noc=onr.noc
-				group by oe.games) re)
-				
+				group by oe.games) re
+				)				
 select games, total_countries 
 	from highest_lowest_participating_countries 
 	where max_rank = 1 or min_rank = 1
@@ -102,43 +103,45 @@ select onr.region as country,count(distinct oe.games) as total_participated_game
 	on oe.noc=onr.noc
 	group by onr.region
 	having count(distinct oe.games)=(select count(distinct games)
-                                 	from olympic_events)                            
+                                 		from olympic_events)                            
  
 --7.Identify the sport which had played in all summer olympics.
 
-with sport_per_games as(select sport,games 
-		                from  olympic_events
-						where season='Summer'
-						group by sport,games
- 						order by games,sport),
- 		
-games_count as(select sport,count(games) as total_games
-				from sport_per_games
- 				group by sport),
- 	
-total_summer_games as(select count(distinct games) as total_games_in_summer 
-						from olympic_events
-			 			where season='Summer')
+with sport_per_games as(
+	select sport,games 
+		from  olympic_events
+		where season='Summer'
+		group by sport,games
+ 		order by games,sport),	
+games_count as(
+	select sport,count(games) as total_games
+		from sport_per_games
+ 		group by sport),
+total_summer_games as(
+	select count(distinct games) as total_games_in_summer 
+		from olympic_events
+		where season='Summer')
 select *
- from games_count
- join total_summer_games
- on games_count.total_games=total_summer_games.total_games_in_summer 
+	from games_count
+	join total_summer_games
+	on games_count.total_games=total_summer_games.total_games_in_summer 
 
 --8.Identify the sport which had played in all winter olympics
 
-with sport_per_games as(select sport,games 
-						 from  olympic_events
-						 where season='Winter'
- 						 group by sport,games
- 						 order by games,sport),
- 		
-games_count as(select sport,count(games) as total_games
-	  			 from sport_per_games
- 	  			 group by sport),
- 	  
-total_winter_games as(select count(distinct games) as total_games_in_winter 
-		 				from olympic_events
-	 					where season='Winter')
+with sport_per_games as(
+	select sport,games 
+		from  olympic_events
+		where season='Winter'
+ 		group by sport,games
+ 		order by games,sport),	
+games_count as(
+	select sport,count(games) as total_games
+		from sport_per_games
+ 		group by sport),
+total_winter_games as(
+	select count(distinct games) as total_games_in_winter 
+		from olympic_events
+	 	where season='Winter')
 select *
 	from games_count
 	 join total_winter_games
@@ -169,15 +172,17 @@ select *
  
 --12.number of male and female athletes participated in all olympic games.
 
-with t1 as(select sex as female,count(sex) as female_athletes
-        	from olympic_events
-       	 	where sex='F'
-       		 group by sex),
-       		 
-t2 as(select sex as male, count(sex) as male_athelets
+with t1 as(
+	select sex as female,count(sex) as female_athletes
         from olympic_events
-        where sex='M'
-        group by sex
+       	where sex='F'
+       	group by sex),
+       		 
+t2 as(
+  select sex as male, count(sex) as male_athelets
+    from olympic_events
+    where sex='M'
+    group by sex
 )
 select *
 from t1,t2
@@ -194,14 +199,16 @@ select sum(case sex when 'F' then 1 else 0 end) as female_athletes, sum(case sex
 
 --13.query to fetch the top 5 athletes who have won the most gold medals.
 
-with t1 as(select name,team,count(name) as total_gold_medals
-	from olympic_events
-	where medal='Gold'
-	group by name,team
-	order by total_gold_medals desc),
+with t1 as(
+	select name,team,count(name) as total_gold_medals
+		from olympic_events
+		where medal='Gold'
+		group by name,team
+		order by total_gold_medals desc),
 	
-t2 as(select *,dense_rank()over (order by total_gold_medals desc) as "top_5_rank"
-    from t1
+t2 as(
+	select *,dense_rank()over (order by total_gold_medals desc) as "top_5_rank"
+    	from t1
 )
 select name,team,total_gold_medals
 	from t2
@@ -209,41 +216,42 @@ select name,team,total_gold_medals
 	
 --13. Improved query--query for all the athelets who won gold medals
 
-with t1 as(select name,team,count(name) as total_gold_medals, dense_rank()over (order by count(name) desc) as "medal_rank"
-        from olympic_events
-        where medal='Gold'
-        group by name,team)
+with t1 as(
+select name,team,count(name) as total_gold_medals, dense_rank()over (order by count(name) desc) as "medal_rank"
+    from olympic_events
+    where medal='Gold'
+    group by name,team)
 select name,team,total_gold_medals,medal_rank
-    	from t1
-    	where medal_rank>='1'
+    from t1
+    where medal_rank>='1'
 
 --14.Query to fetch the top 5 athletes who have won the most medals (Medals include gold, silver and bronze).
 
-with t1 as(select name,team,count(name) as total_medals
-	from olympic_events
-	where medal in('Gold','Silver','Bronze')
-	group by name,team
-order by total_medals desc),
-
-  t2 as(select *,dense_rank() over(order by total_medals desc)as "r"
+with t1 as(
+	select name,team,count(name) as total_medals
+		from olympic_events
+		where medal in('Gold','Silver','Bronze')
+		group by name,team
+		order by total_medals desc),
+t2 as(
+ 	 select *,dense_rank() over(order by total_medals desc)as "r"
       	from t1)
-      	
-	 select name,team,total_medals
+select name,team,total_medals
   	from t2
   	where "r"<='5'
   
 --15. SQL query to fetch the medal status of countries in all olympic games.
 
-with t1 as(select onr.region ,count(medal) as total_medals
-	from olympic_events oe
-	join olympic_noc_regions onr 
-	on oe.noc=onr.noc
-	where medal in ('Gold','Silver','Bronze')
-	roup by onr.region),
-	
-t2 as (select *,dense_rank() over (order by total_medals desc) as "rank"
+with t1 as(
+	select onr.region ,count(medal) as total_medals
+		from olympic_events oe
+		join olympic_noc_regions onr 
+		on oe.noc=onr.noc
+		where medal in ('Gold','Silver','Bronze')
+		group by onr.region),
+t2 as (
+	select *,dense_rank() over (order by total_medals desc) as "rank"
 	    from t1)
-	
 select region,total_medals,"rank"
 	from t2
 	where "rank">='1'
@@ -270,7 +278,6 @@ from olympic_events oe
 	on oe.noc=onr.noc
 	group by onr.region
 	order by Gold_medal desc,Silver_medal desc,Bronze_medal desc)
-	
 select country,Gold_medal,Silver_medal,Bronze_medal
 	from t1
 	where Gold_medal=0 and(Silver_medal>0 or Bronze_medal>0)
@@ -278,21 +285,20 @@ select country,Gold_medal,Silver_medal,Bronze_medal
 
 --18.Medal status in Rio olympics(2016 Summer) (latest olympics in this dataset)
 
-with event_country_medal as (select distinct oe.event "event", onr.region as country, oe.medal as medal
-        from olympic_events oe
-        join olympic_noc_regions onr 
-        on oe.noc=onr.noc and oe.medal != 'NA'
-        where oe.games = '2016 Summer'
-        group by oe.event, country, medal)
+with event_country_medal as (
+select distinct oe.event "event", onr.region as country, oe.medal as medal
+	from olympic_events oe
+    join olympic_noc_regions onr 
+    on oe.noc=onr.noc and oe.medal != 'NA'
+	where oe.games = '2016 Summer'
+    group by oe.event, country, medal)
 select country, sum(case when medal='Gold' then 1 else 0 end) as Gold,
                 sum(case when medal='Silver' then 1 else 0 end) as Silver,
                 sum(case when medal='Bronze' then 1 else 0 end) as Bronze, count (medal) as total_medal
     from event_country_medal
     group by country
     order by Gold desc
-    ----
-    
-   
+
 --19.athelets age over 60 won the gold medal
 
 select name,age,team,sport,games,medal
